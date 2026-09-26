30f2e7e6edf925564e38dbfd3abc20da8a26fb26
UlisesPlaRocher <ulisesplarocher@gmail.com>
2026-09-26 20:12:36 +0200

    asdsad

---
+++ sentry.server.config.ts
@@ -0,0 +1,28 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://082ab12fc2bfca97fe40837e7ade515b@o4512153822822400.ingest.de.sentry.io/4512153881346128',
+
+  // One request in ten. The wizard leaves this at 1, which measures every
+  // single request and burns the free tracing quota long before the month is
+  // out. A tenth is plenty to see which pages are slow and which are not.
+  tracesSampleRate: 0.1,
+
+  // Turns off collection of data that could identify users. Adjust per category:
+  // https://docs.sentry.io/platforms/javascript/configuration/options/#dataCollection
+  dataCollection: {
+    userInfo: false,
+    graphQL: { document: false, variables: false },
+    genAI: { inputs: false, outputs: false },
+    databaseQueryData: false,
+    queues: false,
+    httpBodies: [],
+    httpHeaders: { deny: ['forwarded', '-ip', 'remote-', 'via', '-user'] },
+    cookies: { deny: ['forwarded', '-ip', 'remote-', 'via', '-user'] },
+    urlQueryParams: { deny: ['forwarded', '-ip', 'remote-', 'via', '-user'] },
+  },
+});
